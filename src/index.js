def93e6a5a97d396db834fca6db3917a7826c51f
h-ito <Hiroshi.Ito@nabiace.co.jp>
2023-11-22 16:08:26 +0900

    add RubicCube.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import App from './cannon-test'
+//import Rubic from './RubicCube'
 
 createRoot(document.getElementById('root')).render(<App />)
